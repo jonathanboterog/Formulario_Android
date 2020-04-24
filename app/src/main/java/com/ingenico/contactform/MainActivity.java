899cfe5c4eadc846b143a915798bdebab1cc069b
USRINGENICO\jbotero <jonathan.botero@ingenico.com>
2020-04-24 18:50:24 -0400

    + Se agrega actividades y flujo de aplicacion de acuerdo a requerimientos de la tarea solicitada.

--- app/src/main/java/com/ingenico/contactform/MainActivity.java
+++ app/src/main/java/com/ingenico/contactform/MainActivity.java
@@ -2,13 +2,66 @@ package com.ingenico.contactform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String name;
+    private String date;
+    private String phone;
+    private String email;
+    private String description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnClickNext(View view) {
+        Intent intent = new Intent(this, ShowContact.class);
+
+        //--- name
+        EditText edt_name = (EditText)findViewById(R.id.edt_name);
+        name = edt_name.getText().toString().trim();
+
+        //--- Date
+        DatePicker pick_date = (DatePicker)findViewById(R.id.pick_date);
+        int   day  = pick_date.getDayOfMonth();
+        int   month= pick_date.getMonth();
+        int   year = pick_date.getYear();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        date = sdf.format(calendar.getTime());
+
+        //--- phone
+        EditText edt_phone = (EditText)findViewById(R.id.edt_phone);
+        phone = edt_phone.getText().toString().trim();
+
+        //--- email
+        EditText edt_email = (EditText)findViewById(R.id.edt_email);
+        email = edt_email.getText().toString().trim();
+
+        //--- Description
+        EditText edt_description = (EditText)findViewById(R.id.edt_description);
+        description = edt_description.getText().toString().trim();
+
+        intent.putExtra(String.valueOf(R.string.pform_name), name);
+        intent.putExtra(String.valueOf(R.string.pform_date), date);
+        intent.putExtra(String.valueOf(R.string.pform_phone), phone);
+        intent.putExtra(String.valueOf(R.string.pform_email), email);
+        intent.putExtra(String.valueOf(R.string.pform_description), description);
+
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/ingenico/contactform/ShowContact.java
@@ -0,0 +1,55 @@
+package com.ingenico.contactform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class ShowContact extends AppCompatActivity {
+
+    private String name;
+    private String date;
+    private String phone;
+    private String email;
+    private String description;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_contact);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            name = extras.getString(String.valueOf(R.string.pform_name));
+            date = extras.getString(String.valueOf(R.string.pform_date));
+            phone = extras.getString(String.valueOf(R.string.pform_phone));
+            email = extras.getString(String.valueOf(R.string.pform_email));
+            description = extras.getString(String.valueOf(R.string.pform_description));
+        }
+
+        //--- Set name
+        TextView tv_name = findViewById(R.id.tv_name);
+        tv_name.setText(name);
+
+        //--- Set Date
+        TextView tv_date = findViewById(R.id.tv_date);
+        tv_date.setText(tv_date.getText() + " " +  date);
+
+        //--- Set phone
+        TextView tv_phone = findViewById(R.id.tv_phone);
+        tv_phone.setText(tv_phone.getText() + " " +  phone);
+
+        //--- Set Email
+        TextView tv_email = findViewById(R.id.tv_email);
+        tv_email.setText(tv_email.getText() + " " +  email);
+
+        //--- Set description
+        TextView tv_description = findViewById(R.id.tv_description);
+        tv_description.setText(tv_description.getText() + " " + description);
+    }
+
+    public void OnClickEdit(View view) {
+        finish();
+    }
+}
